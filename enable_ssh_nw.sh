91671d7387dd00b9b4c3226689db85b601c4bdcf
Ajey Gore <ajey@ajeys-mbp.lan>
2022-05-27 23:01:09 +0800

    Added more scripts

--- enable_ssh_nw.sh
+++ enable_ssh_nw.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# A sample Bash script, by Ryan
 echo "$1 "
 if [ $# -eq 0 ]; then
     echo "No arguments provided"
     echo "Please provide RPI mount path usually /Volumes/boot"
     exit 1
 fi
+
+#enabling ssh
+echo "Enabling SSH, creating file ssh"
+touch $1/ssh
+
+
