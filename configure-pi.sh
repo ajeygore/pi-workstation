f55d049fabd78231bbcd04090f8e5fddca3e5507
ajey <ajeygore@gmail.com>
2022-06-09 21:44:38 +0800

    removed networking playbook, people with pi4 can run one extra command

--- configure-pi.sh
+++ configure-pi.sh
@@ -7,5 +7,5 @@ localedef -i en_US -f UTF-8 en_US.UTF-8 && /usr/sbin/update-locale LANG=$LANG
 wget https://raw.githubusercontent.com/ajeygore/pi-workstation/main/rpi.yml
 sudo ansible-playbook rpi.yml 
 #Get networking sh yml
-wget https://raw.githubusercontent.com/ajeygore/pi-workstation/main/rpi-networking.yml
-sudo ansible-playbook rpi-networking.yml 
+#wget https://raw.githubusercontent.com/ajeygore/pi-workstation/main/rpi-networking.yml
+#sudo ansible-playbook rpi-networking.yml 
--- rpi-network.sh
+++ rpi-network.sh
@@ -4,4 +4,4 @@ apt-get update && apt-get install -yqq locales lsb-release software-properties-c
 LANGUAGE=en_US.UTF-8
 LANG=en_US.UTF-8
 localedef -i en_US -f UTF-8 en_US.UTF-8 && /usr/sbin/update-locale LANG=$LANG
-ansible-playbook rpi.yml 
+ansible-playbook rpi_networking.yml 
