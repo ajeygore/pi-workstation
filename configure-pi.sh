0252363be2098df12d8f1c15d41384fef742d021
Ajey Gore <ajey@ajeys-mbp.lan>
2022-06-11 08:34:44 +0800

    Fixing sudo issue

--- configure-pi.sh
+++ configure-pi.sh
@@ -5,7 +5,7 @@ LANG=en_US.UTF-8
 localedef -i en_US -f UTF-8 en_US.UTF-8 && /usr/sbin/update-locale LANG=$LANG
 #Get devlopment environment yml
 wget https://raw.githubusercontent.com/ajeygore/pi-workstation/main/rpi.yml
-sudo ansible-playbook rpi.yml 
+sudo RPI_USER=$RPI_USER ansible-playbook rpi.yml 
 #Get networking sh yml
 wget https://raw.githubusercontent.com/ajeygore/pi-workstation/main/rpi-networking.yml
 sudo ansible-playbook rpi-networking.yml 
