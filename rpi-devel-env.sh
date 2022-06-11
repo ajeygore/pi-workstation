ebe72513bed5aaf333e6326d1ed8a718a0f7fe0d
Ajey Gore <ajeygore@gmail.com>
2022-06-11 13:45:39 +0800

    Update rpi-devel-env.sh

--- rpi-devel-env.sh
+++ rpi-devel-env.sh
@@ -5,4 +5,4 @@ LANGUAGE=en_US.UTF-8
 LANG=en_US.UTF-8
 localedef -i en_US -f UTF-8 en_US.UTF-8 && /usr/sbin/update-locale LANG=$LANG
 wget https://raw.githubusercontent.com/ajeygore/pi-workstation/main/rpi.yml
-sudo ansible-playbook rpi.yml 
+sudo RPI_USER=$RPI_USER ansible-playbook rpi.yml 
