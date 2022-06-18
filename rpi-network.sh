3cf9b9a6719e6b03f5cabacbf847a0fdf796d09f
Ajey Gore <ajeygore@gmail.com>
2022-06-18 09:14:50 +0800

    Update rpi-network.sh

--- rpi-network.sh
+++ rpi-network.sh
@@ -1,7 +1,8 @@
 
 #!/bin/sh
-apt-get update && apt-get install -yqq locales lsb-release software-properties-common ansible && apt-get clean  
+apt-get update && apt-get install -yqq locales lsb-release software-properties-common ansible wget && apt-get clean  
 LANGUAGE=en_US.UTF-8
 LANG=en_US.UTF-8
 localedef -i en_US -f UTF-8 en_US.UTF-8 && /usr/sbin/update-locale LANG=$LANG
-ansible-playbook rpi.yml 
+wget https://raw.githubusercontent.com/ajeygore/pi-workstation/main/rpi-networking.yml
+sudo ansible-playbook rpi_networking.yml 
